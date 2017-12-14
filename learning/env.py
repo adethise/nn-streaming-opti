3e0a718dae81939a4353513e89dd8a0ff86d470b
Arnaud Dethise <arnaud.dethise@kaust.edu.sa>
2017-12-14 18:28:02 +0300

    nnopti-stormbenchmark interface base

--- learning/env.py
+++ learning/env.py
@@ -1,16 +1,49 @@
-import time
+import yaml
 import random
+import os
+
+ACTION_LABELS = (
+        'topology.workers', 'topology.acker.executors',
+        'topology.max.spout.pending', 'component.spout_num',
+        )
+
+ACTIONS = [
+        (12, 12, 5000,  6), ( 9,  6, 7000,  6), (12,  3, 1000,  3),
+        ( 6, 12, 1000,  6), (12,  3, 5000,  6), ( 9, 12, 3000,  9),
+        (12,  9, 3000, 12), (12,  3, 5000,  9), (12,  3, 3000,  9),
+        ( 6,  6, 1000, 12), ( 6, 12, 7000, 12), (12, 12, 7000,  9),
+        ( 6,  6, 7000, 12), ( 9,  9, 5000,  9), ( 9, 12, 1000,  3),
+        ( 9, 12, 7000,  3), ( 9, 12, 3000, 12), (12,  6, 3000, 12),
+        ( 9,  3, 5000,  9), (12,  3, 3000,  6),
+        ]
+
+JOBS = {
+        'RollingCount': 'rollingcount.yaml',
+        'RollingTopWords': 'rollingtopwords.yaml',
+        'SentimentAnalysis': 'sentiment.yaml',
+        }
+
+CONFIG_DIR = 'yamlconfs/'
 
 class Simulator:
 
     def __init__(self, random_seed = 42):
         random.seed(random_seed)
+        self.next_job = 'RollingCount'
 
-    def get_performance(self, action):
+    def get_performance(self, action_index):
+        self.run_evaluation(ACTIONS[action_index])
 
-        if action == 'A':
-            return random.random() * 100, 100, 100
-        elif action == 'B':
-            return random.random() * 200, 100, 100
-        else:
-            return random.random() * 300, 100, 100
+    def write_config(self, job, action):
+        conf = yaml.load(open(CONFIG_DIR + JOBS[job]))
+        for label, value in zip(ACTION_LABELS, action):
+            conf[label] = value
+        yaml.dump(conf, open(CONFIG_DIR + JOBS[job], 'w'), default_flow_style = False)
+
+    def run_evaluation(self, action):
+        self.write_config(self.next_job, action)
+        os.environ['TOPOLOGY'] = self.next_job
+        os.environ['CONF'] = JOBS[self.next_job]
+        os.system('cp %s ~/.storm/' % JOBS[self.next_job])
+        os.chdir('/home/ubuntu/stormbenchmark/tuning')
+        os.system('bash run_nnopti.sh 1')

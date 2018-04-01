169a3dcf26a5ac2c63bb2797de092e1617db94d5
Arnaud Dethise <arnaud.dethise@kaust.edu.sa>
2018-04-01 18:59:45 +0300

    fixed typo

--- learning/main.py
+++ learning/main.py
@@ -98,7 +98,7 @@ def reward_function(metrics):
     Compute the reward based on metric information about a run.
     This function defines the target QoS.
     '''
-    return -metrics[latency][0]
+    return -metrics['full_latency'][0]
 
 
 if __name__ == '__main__':
